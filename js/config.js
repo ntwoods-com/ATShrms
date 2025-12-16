18a0352899138ef0ec75ae72a9510b07a4cbc17c
SONIC <singhrajesh41273@gmail.com>
2025-12-16 15:46:32 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwYMXKuVckPwC8P0SqjvwIwzqZRV5NbsGTUTNKlAuNfLLyXHpDtXJZidrzSGSTRA3SxEw/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbzuIAMnhAn1JbTyw5G89ZRRYlUM9xW2uK-4YUuENSvNJaRcJqrknsTn3MazEc1MWt87/exec",
   GOOGLE_CLIENT_ID: "1029752642188-ku0k9krbdbsttj9br238glq8h4k5loj3.apps.googleusercontent.com"
 };
